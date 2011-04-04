4701d727b66f0bec2951fb636c09af364a80ecb2
Ravi Khadiwala <ravi.khadiwala@gmail.com>
2011-04-04 16:57:03 -0500

    fixed listener

--- listener.cpp
+++ listener.cpp
@@ -6,109 +6,138 @@
 #include <string>
 #include <sstream>
 #include "mp2_sha1-c/sha1.h"
+#include <math.h>
+
 #define INTROPORT 4325
 using namespace std;
-int m;
-void parseSend(string line,int * i,int sockfd){
-    //Options are ADD_LINE, ADD_FILE, ADD_NODE, GET_TABLE, SLEEP, DEL_FILE, FIND_FILE
-    stringstream ss (stringstream::in | stringstream::out);
-    string tosend;
-    ss << line;
-    string token;
-    ss >> token;
-    
-    //Token is now the first word in the file
+//global variables
+int portNumOffset;
 
-	cout << "Token is "<<token<<endl;
-    if (!token.compare("ADD_FILE")){
-	ss >> token;
-	SHA1Context sha;
-	SHA1Reset(&sha);
-	//unsigned char * tmp = new unsigned char[token.size() + 1];
-	//strcpy(tmp, token.c_str());
-	SHA1Input(&sha, (const unsigned char*)token.c_str(), token.size());
-	if(!SHA1Result(&sha))
-		fprintf(stderr, "could not hash\n");
-	int fileID = sha.Message_Digest[4]%((int)(1<<m));
-	cout<<"fileID for "<<token<<" is : "<<fileID<<endl;
-        cout<<"trying to add file"<<endl;
-        tosend.append("findID,"); //findID,<fileID>,addFile,<fileName>,<ip>
-	tosend.append(itoa(fileID));
-	tosend.append(",addFile,");
-	tosend.append(token);
-	tosend.append(",");
-	ss >> token;
-	tosend.append(token);
-	//char * tmp = new char[tosend.size() + 1];
-	//strcpy(tmp, tosend.c_str());
-	s_send(sockfd, (char *)(tosend.c_str()));
-	//delete tmp;
-	
-     }
-
-	else if (!token.compare("ADD_NODE")){
-            cout<<"trying to add node"<<endl;
-            //tosend.append("addnew,");
-            while( ss >> token){
-                //tosend.append(token);
-		sleep(1);
-                //s_send(token
-                char tmp [tosend.size() + 1];
-                strcpy(tmp,"addnew,");
-                strcat(tmp,token.c_str());
-                strcat(tmp,",");
-                strcat(tmp,itoa(INTROPORT+ *i));        
-		//strcat(tmp,",");
-		printf("tmp --- %s\n", tmp);
-                s_send(sockfd,tmp);
-                (*i)++;
-            }
-        }
-	else if (!token.compare("GET_TABLE")){
-            cout<<"trying to get table"<<endl;
-            tosend.append("gt;");
-            while( ss >> token){
-                tosend.append(token);
-                tosend.append(";");
-            }
-        }
-
-	else if (!token.compare("SLEEP")){
-            cout<<"trying to sleep"<<endl;
-            tosend.append("sl;");
-            while( ss >> token){
-                tosend.append(token);
-                tosend.append(";");
-            }
-        }
-
-	else if (!token.compare("DEL_FILE")){
-            cout<<"trying to del file"<<endl;
-            tosend.append("df;");
-            while( ss >> token){
-                tosend.append(token);
-                tosend.append(";");
-            }
-        }
-	else if (!token.compare("FIND_FILE")){
-            cout<<"trying to find file"<<endl;
-            tosend.append("ff;");
-            while( ss >> token){
-                tosend.append(token);
-                tosend.append(";");
-            }
-        }
-	else{
-		cout<<"ruh roh"<<endl;
-	}
-	
-	
-	tosend = tosend.substr(0,tosend.length() -1);
-    cout <<"sending "<< tosend<<endl;
-
-
-
+int hashFileName(char * token,int m)
+{
+    SHA1Context sha;
+    SHA1Reset(&sha);
+    SHA1Input(&sha,(unsigned char*) token, strlen(token));
+    if(!SHA1Result(&sha))
+    	fprintf(stderr, "could not hash\n");
+    return sha.Message_Digest[4]%(1<<m);
 }
+
+//returns true if you should keep parsing
+bool parseSend(char * line,int sockfd,bool isFirst,int m)
+{
+    bool ret = false;
+    char * token = strtok(line," \n");
+    if(strcmp(token,"ADD_FILE") == 0)
+    {
+        token = strtok(NULL," \n"); //token = filename
+	    int fileID = hashFileName(token,m);
+        char commandmsg[256];
+        strcpy(commandmsg,"findID,");   //findID,<fileID>,addFile,<filename>,<ip adress>
+        strcat(commandmsg,itoa(fileID));
+        strcat(commandmsg,",addFile,");
+        strcat(commandmsg,token);
+        strcat(commandmsg,",");
+        token = strtok(NULL," \n");         //token = ip address
+        strcat(commandmsg,token);
+        printf("%s\n",commandmsg);
+        ret = true;
+	    s_send(sockfd, commandmsg);
+    }
+    else if(strcmp(token,"ADD_NODE") == 0)
+    {
+        char commandmsg[256];
+        while((token = strtok(NULL," \n")) != NULL)
+        {
+            //sleep(1);
+            strcpy(commandmsg,"addnew,");
+            strcat(commandmsg,token);
+            strcat(commandmsg,",");
+            strcat(commandmsg,itoa(INTROPORT + portNumOffset));
+            portNumOffset++;
+            printf("%s\n",commandmsg);
+            s_send(sockfd, commandmsg);
+        }
+        ret = true;
+    }
+    else if(strcmp(token,"GET_TABLE") == 0)
+    {
+        token = strtok(NULL," \n"); //token = filename
+        int fileID = hashFileName(token,m);
+        char commandmsg[256];
+        strcpy(commandmsg,"findID,");   //findID,<fileID>,getTable,<filename>
+        strcat(commandmsg,itoa(fileID));
+        strcat(commandmsg,",getTable,");
+        strcat(commandmsg,token); 
+        printf("%s\n",commandmsg);
+        s_send(sockfd,commandmsg);
+        ret = true;
+    }
+    else if(strcmp(token,"SLEEP") == 0)
+    {
+        token = strtok(NULL," \n");
+        sleep(atoi(token));
+        ret = true;
+    }
+    else if(strcmp(token,"DEL_FILE") == 0)
+    {
+        token = strtok(NULL," \n"); //token = filename
+	    int fileID = hashFileName(token,m);
+        char commandmsg[256];
+        strcpy(commandmsg,"findID,");   //findID,<fileID>,delFile,<filename>
+        strcat(commandmsg,itoa(fileID));
+        strcat(commandmsg,",delFile,");
+        strcat(commandmsg,token);
+        printf("%s\n",commandmsg);
+        s_send(sockfd,commandmsg);
+        ret = true;
+    }
+    else if(strcmp(token,"FIND_FILE") == 0)
+    {
+        token = strtok(NULL," \n"); //token = filename
+	    int fileID = hashFileName(token,m);
+        char commandmsg[256];
+        strcpy(commandmsg,"findID,");   //findID,<fileID>,findFile,<filename>
+        strcat(commandmsg,itoa(fileID));
+        strcat(commandmsg,",findFile,");
+        strcat(commandmsg,token);
+        printf("%s\n",commandmsg);
+        s_send(sockfd,commandmsg);
+        ret = true;
+    }
+    else if(strcmp(token,"QUIT") == 0)
+    {
+        printf("Quit message not implemented\n");
+        exit(0);
+    }
+    else if(isFirst)
+    {
+        FILE * f = fopen(token,"r");
+        char command[256];
+        if(f == NULL)
+        {
+            printf("Not a valid command or file name, try again  >> ");
+            fgets(command,sizeof(command),stdin);
+            ret = parseSend(command,sockfd,true,m);
+        }
+        else
+        {
+            while(fgets(command,sizeof(command),f) != NULL)
+            {
+                printf("read command:%s",command);
+                parseSend(command,sockfd,false,m);
+            }
+            ret = false;
+        }
+    }
+    else
+    {
+        printf("Invalid command %s\n",token);
+        ret = true;
+    }
+    return ret;
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -117,78 +146,29 @@ int main(int argc, char* argv[])
         exit(1);
     }
     int m = atoi(argv[1]);
-	cout<<"listener started with " << m << " nodes"<<endl;
-
-    cout << "would you like to use a [F]ile or [T]erminal to input commands?" << endl;
-    string mode = "";
-    cin >> mode;
     if(fork() == 0)
     {
         Introducer * introducer = new Introducer(0,INTROPORT,m);
-        //(*introducer).addNewNode(18,INTROPORT+1);  //test,normally this is called from handler
-        //sleep(1);
-        //(*introducer).addNewNode(10,INTROPORT+2);
         sleep(100); //hack
     }    
     
+    sleep(2); //allow introducer to start
     //Listener functions here
+	cout<<"listener started with " << m << " nodes"<<endl;
     int lsock = new_socket();
-    //ifstream listenerFile; 
-    FILE * f;
-    string line;
+    portNumOffset = 1;
 
-    if(!mode.compare("T") || !mode.compare("t")){
-	int i = 1;
-	char command[256];
-	connect(lsock,INTROPORT);
-        while(mode.compare("exit")){
-	    
-            cout << "type command or \"exit\" to quit" << endl;
-	    //cin.getline(command,256);
-	    scanf("%s", command);
-	    cout<<"terminal sent " << command<<endl;
-            parseSend(command,&i,lsock);
-        }
-	
+    char command[256];
+    connect(lsock,INTROPORT);
+    printf("input command or file name >> ");
+    fgets(command,sizeof(command),stdin);
+    bool isFirst = true;
+    while(parseSend(command,lsock,isFirst,m))
+    {    
+        isFirst = false;
+        printf("Input next command >> ");                 
+        fgets(command,sizeof(command),stdin);
     }
-    else if (!mode.compare("F") || !mode.compare("f")){
-	   cout << "Enter Filename" <<endl;
-	   //cin >> mode;
-	   cin >> mode;
-	   cout<<mode;
-	   f = fopen((char *)mode.c_str(), "r");
-	   char buf[256];
-	   int i = 1;
-	   connect(lsock,INTROPORT);
-	   while(fgets(buf, 256, f) != NULL){
-		//printf("%s \n", buf);
-		line = buf;
-		parseSend(line,&i,lsock);
-	   }
-	   
-	   //listenerFile.open(buf);
-	   ////f = fopen(mode.c_str(),"r");
-	   //char* buf[256];
-	   // if (listenerFile.is_open()){//f = NULL){
-	   //     int i = 1;
-	   //     connect(lsock,INTROPORT);
-	   //     while ( listenerFile.//fread(buf,256,256,f) != 0 ){
-	   //         //getline(f,line);
-	   //         line = buf;
-	   //         parseSend(line,&i,lsock);
-	   //         sleep(2);
-	   //         //cout<<"ListenerFile read "<<line<<endl;
-	   //     }
-	   //     //f.close();
-	   //     fclose(f);
-	   //     sleep(100);
-	   // }
-    //cout << "Failed to open File" << endl;
     sleep(100);
-	
-    
-    return 0; 
-    }
+    return 0;
 }
-
-
